cbe7fbd9e197a0e488ee82d2c4f1490aec42c056
riggi <simone.riggi@gmail.com>
2019-06-12 13:40:24 +0200

    Add getBeamCentre script

--- scripts/getBeamCentre.sh
+++ scripts/getBeamCentre.sh
@@ -11,8 +11,8 @@ BEAM_ID=$2
 echo "FOOTPRINT_FILE: $FOOTPRINT_FILE"
 echo "BEAM_ID: $BEAM_ID"
 
-line=`awk '$1==$BEAM_ID' $FOOTPRINT_FILE`
-echo $line
+line=`awk '$1=='"$BEAM_ID" $FOOTPRINT_FILE`
+echo "line: $line"
 
 ra_field=$(echo $line | sed -e 's/,/ /g' | sed -e 's/(//g' | sed -e 's/)//g'| awk '{print $4}')
 ra=$(echo "$ra_field" | awk -F':' '{printf "%sh%sm%s",$1,$2,$3}')
